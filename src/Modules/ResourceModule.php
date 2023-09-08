f1b5c12eda9ea309e63f4fa885e09558c0d2d91c
Mohamedkhalil1 <mohamedkhalil125445@gmail.com>
2023-09-08 19:51:21 +0300

    refactor

---
+++ src/Modules/ResourceModule.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Loffy\CreateLaravelModule\Modules;
+
+use Exception;
+use Illuminate\Support\Facades\File;
+use Loffy\CreateLaravelModule\DTOs\ModuleDTO;
+
+class ResourceModule
+{
+    public function __construct(private ModuleDTO $dto) {}
+
+    public static function make(ModuleDTO $dto): static
+    {
+        return new static($dto);
+    }
+
+    public function handle()
+    {
+        $resourceName = "{$this->dto->getBaseModelName()}Resource";
+        $resourceDir = base_path("app/Http/Resources/{$this->dto->getNamespace()}");
+        $thisString = '$this';
+        $resources = $this->dto->getColumns()
+            ->map(fn ($column) => "            '$column->COLUMN_NAME' => $thisString->$column->COLUMN_NAME,")
+            ->join(PHP_EOL);
+
+        $resource = File::get(__DIR__ . '/../Commands/stubs/DummyResource.stub');
+        $resource = str_replace('DummyNamespace', $this->dto->getNamespace(), $resource);
+        $resource = str_replace('DummyResource', "{$this->dto->getBaseModelName()}Resource", $resource);
+        $resource = str_replace('ResourcesArray', $resources, $resource);
+        $resource = str_replace('DummyModel', $this->dto->getBaseModelName(), $resource);
+        if (File::exists($resourceDir . "/$resourceName.php")) {
+            throw new Exception("Resource $resourceName already exist in $resourceDir!");
+        }
+        if (!File::exists($resourceDir)) {
+            File::makeDirectory($resourceDir, recursive: true);
+        }
+        File::put($resourceDir . "/$resourceName.php", $resource);
+    }
+}
---
+++ src/Modules/RouteModule.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Loffy\CreateLaravelModule\Modules;
+
+use Illuminate\Support\Facades\File;
+use Loffy\CreateLaravelModule\DTOs\ModuleDTO;
+
+class RouteModule
+{
+
+    public function __construct(private ModuleDTO $dto) {}
+
+    public static function make(ModuleDTO $dto): static
+    {
+        return new static($dto);
+    }
+
+    public function handle(): void
+    {
+        $routes = [
+            "Route::resource('{$this->dto->getSingularSnakeCaseTitle()}', \\App\\Http\\Controllers\\{$this->dto->getNamespace()}\\{$this->dto->getBaseModelName()}Controller::class);",];
+        File::append(base_path('routes/api.php'), PHP_EOL . implode(PHP_EOL, $routes));
+    }
+
+}
